78ccbed15c596d9d18f3b8b71635fb617117839e
kilometersvi <milesmilosevich@gmail.com>
2022-10-08 01:42:07 -0700

    radialassist

--- src/start.py
+++ src/start.py
@@ -1,9 +1,36 @@
 import os
 import time
+from Dongleless-myo.dongleless import dongleless
+
+def unknown(myo):
+    print("unknown")
+
+def arm_synced(myo, x_dir, arm):
+    print("arm_synced")
+
+def arm_unsynced(myo):
+    print("arm_unsynced")
+
+def imu_data(myo, quat, accel, gyro):
+    print("imu_data")
+
+def emg_data(myo, emg):
+    print("emg_data")
 
 def main():
     print(f"### {time.asctime(time.localtime())} ###")
     print("Running RadialAssist service v0.1...")
+
+
+    function_dict = {
+        "unknown":unknown,
+        "arm_synced":arm_synced,
+        "arm_unsynced":arm_unsynced,
+        "imu_data":imu_data,
+        "emg_data":emg_data
+    }
+
+    dongleless.run(function_dict)
 
 if __name__ == "__main__":
     main()
